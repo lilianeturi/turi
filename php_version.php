48e74c7c63d85fe436e9ff098782e75ed4b83082
lilianeturi <turikumwenimanaliliane@gmail.com>
2025-12-24 20:50:03 +0800

    Add files via upload

---
+++ php_version.php
@@ -0,0 +1,7 @@
+<?php
+
+echo phpVersion()."&nbsp";
+//phpinfo();
+echo("this one is mayibobo")."&nbsp"."liliane";
+
+?>
---
+++ sum.php
@@ -0,0 +1,11 @@
+<?php
+$sum = 0;
+for ($num = 0; $num <= 10; $num++) {
+    echo $num . "<br>";
+    $suma=$sum += $num;       
+}
+echo "the sum is " . $suma . "<br>";
+$st_name="igiraneza";
+$nd_name="fortune";
+echo $st_name."&nbsp" .$nd_name;
+?>
